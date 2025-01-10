cbf62b5a8ba06aac1383b3844f54717ccff13e78
Calebe Arlan Souza da Silva <calebearlan@gmail.com>
2025-01-09 22:17:46 -0300

    feat Classe Musica

--- UnderSound/Program.cs
+++ UnderSound/Program.cs
@@ -1,1 +1,16 @@
-﻿Musica muscica1 = new Musica();
+﻿Musica musica1 = new Musica();
+musica1.nome = "For Those Who Wait";
+musica1.artista = "Fireflight";
+musica1.duracao = 340;
+musica1.disponivel = true;
+
+Musica musica2 = new Musica();
+musica2.nome = "Debaixo da Terra";
+musica2.artista = "Os Under-Undergrounds";
+musica2.duracao = 290;
+musica2.disponivel = false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+
+
